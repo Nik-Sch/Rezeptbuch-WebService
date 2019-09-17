b184d19873e9ae6e5355236d1bbe2a2c2e0cbab2
Nik-Sch <nik.schelten@gmail.com>
2019-09-17 09:24:39 +0000

    better deployment

--- startService.sh
+++ startService.sh
@@ -2,5 +2,5 @@
 cd /opt/webservice/rezeptbuch/
 mv service.log service.log.bak
 . venv/bin/activate
-python app.py 2>&1 | tee service.log
+gunicorn --workers=4 -b 0.0.0.0:5425 --access-logfile - app:app |& tee service.log
 deactivate
